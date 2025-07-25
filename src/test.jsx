011d27119d40629503bdc1245f820acd66cae887
liwei <liwei@liyi.co>
2025-07-25 08:59:18 +0800

    clean

--- src/test.jsx
+++
@@ -1,3 +0,0 @@
-/// <reference types="types-for-adobe/Photoshop/2015.5"/>
-
-stringIDToTypeID("test");
--- typings/ExtendScript.d.ts
+++
@@ -1,4 +0,0 @@
-declare function stringIDToTypeID(s: string): number;
-declare function charIDToTypeID(s: string): number;
-declare function executeActionGet(ref: ActionReference): ActionDescriptor;
-declare function executeAction(id: number, desc: ActionDescriptor, dialogMode: DialogModes): ActionDescriptor;
